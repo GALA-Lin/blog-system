316a0a0b9e4fd9930c904866a59f8dc44eaae598
Linsen HU <1563883475@qq.com>
2025-10-07 16:40:14 +0800

    feat: scan module mapper

--- src/main/java/com/blog/BlogSystemApplication.java
+++ src/main/java/com/blog/BlogSystemApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@MapperScan("com.blog.mapper")
+@MapperScan("com.blog.**.mapper")
 @EnableCaching
 @EnableAsync
 @EnableScheduling
