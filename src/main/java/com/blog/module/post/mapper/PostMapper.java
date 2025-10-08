d843ec28e2e21e8291d22c027d24466f24ebcf8d
Linsen HU <1563883475@qq.com>
2025-10-08 13:51:21 +0800

    feat: init PostMapper

---
+++ src/main/java/com/blog/module/post/mapper/PostMapper.java
@@ -0,0 +1,15 @@
+package com.blog.module.post.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.blog.entity.Post;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author: GALA_Lin
+ * @Date: 2025-10-08-13:26
+ * @Description:
+ */
+@Mapper
+public interface PostMapper extends BaseMapper<Post> {
+
+}
